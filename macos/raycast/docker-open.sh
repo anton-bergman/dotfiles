1e7b39d00988ad86d20b4b24d0fef872334bc646
Anton Bergman <anton.u.bergman@gmail.com>
2025-11-08 19:48:01 +0100

    Add open docker script to raycast config

---
+++ macos/raycast/docker-open.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Docker Desktop
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon /Applications/Docker.app/Contents/Resources/AppIcon.icns
+
+# Documentation:
+# @raycast.description Open Docker Desktop while its running in the background
+# @raycast.author Anton Bergman
+
+open "/Applications/Docker.app/Contents/MacOS/Docker Desktop.app"
